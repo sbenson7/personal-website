fefffcd3aaf0477fc03bac014b010296171ff04c
Scott Benson <scott.benson96@gmail.com>
2018-07-16 19:30:00 -0400

    add remaining images and a bit of text

--- react-client/src/components/pages/funScott.js
+++ react-client/src/components/pages/funScott.js
@@ -14,14 +14,27 @@ class FunScott extends Component {
 
 		        Hi, I'm fun Scott!
 		      </div>
-		       <div className='funheaders'>
-		      	 I love playing and watching soccer. My favorite team is Tottenham Hotspur, a soccer team in the English Premier League.
+		      <div className='pic'>
+			    <img src="https://s3.amazonaws.com/scott-personal-website/spurs_scott.jpg"/>
+			  </div>
+		      <div className='fun-description'>
+		      	I love playing and watching soccer. My favorite team is Tottenham Hotspur, a soccer team in the English Premier League.
 		      </div>
+		      <br> 
+		      </br>
 		      <div className='pic'>
 			    <img src="https://s3.amazonaws.com/scott-personal-website/britishscott.jpg"/>
 			  </div>
 		      <div className='fun-description'>
-		      	I’m half British! My dad’s from England, and is a huge reason for my love of soccer and Tottenham Hotspur.
+		      	I’m half British! My dad’s from England, and is a huge reason for my love of soccer(football) and Tottenham Hotspur.
+		      </div>
+		      <br> 
+		      </br>
+		       <div className='pic'>
+			    <img src="https://s3.amazonaws.com/scott-personal-website/scottanimals.jpg"/>
+			  </div>
+		      <div className='fun-description'>
+		      	I love animals, especially cats and dogs. My family has 4 cats, even though i'm allergic to them (I take a bunch of allergy medication.) I also volunteer at a couple of animal shelters!
 		      </div>
 		      <br> 
 		      </br>
@@ -29,7 +42,7 @@ class FunScott extends Component {
 			    <img src="https://s3.amazonaws.com/scott-personal-website/scott76.jpg"/>
 			  </div>
 		      <div className='fun-description'>
-		      	I have a love/hate relationship with the video game Overwatch.
+		      	I have a love/hate relationship with the video game Overwatch. 
 		      </div>
 		    </div>
         </div>    
--- react-client/src/components/pages/workScott.js
+++ react-client/src/components/pages/workScott.js
@@ -14,6 +14,14 @@ class WorkScott extends Component {
 		       		Hi, I'm work Scott!
 		        </div>
 		        <div className='pic'>
+					<img src="https://s3.amazonaws.com/scott-personal-website/studyclashscott.jpg"/>
+				</div>
+			    <div className='work-description'>
+			    	I cofounded Study Clash, an educational website and app which converts notes into multiple-choice trivia games so students can learn classroom material as they compete with their friends.
+			    </div>
+			    <br>
+			    </br>
+		        <div className='pic'>
 					<img src="https://s3.amazonaws.com/scott-personal-website/langtoncreativescott.jpg"/>
 				</div>
 			    <div className='work-description'>
@@ -42,6 +50,14 @@ class WorkScott extends Component {
 				</div>
 			    <div className='work-description'>
 			    	I graduaded in May 2018 from Binghamton University, where I majored in Political Science and minored in graphic design. While I'm still interested in Political Science, my graphic design courses seem more relevant for my current career path. Here's some work I did in my classes! 
+			    </div>
+			    <br>
+			    </br>
+			    <div className='pic'>
+					<img src="https://s3.amazonaws.com/scott-personal-website/winkingscott.jpg"/>
+				</div>
+			    <div className='work-description'>
+			    	Want to see basically just a boring version of this page? Check out my resume here!
 			    </div>
 			</div>
         </div>    
