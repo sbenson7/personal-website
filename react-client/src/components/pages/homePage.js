70e2f493a1fd2dc0820f845729b54ae154d9d00a
Scott Benson <scott.benson96@gmail.com>
2018-07-19 16:41:43 -0400

    start making responsive

--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import FaInstagram from 'react-icons/lib/fa/instagram';
 import FaTwitter from 'react-icons/lib/fa/twitter';
@@ -17,36 +18,70 @@ class HomePage extends Component {
   render() {
     return (
     	<div>
-	        <div className='welcome'>
-	        	Hi, I'm Scott!
-	        </div>
-	        <div className='center'>
-		    	<div className='fpic'>
-		      		<img src="https://s3.amazonaws.com/scott-personal-website/fun_and_work_scott.jpg"/>
+    		<MediaQuery minDeviceWidth={900}>
+		        <div className='welcome'>
+		        	Hi, I'm Scott!
 		        </div>
-		    	<br> 
-		   		</br>
-		        <div className='funscott'>
-		        	<button className="fbttn" onClick={() => this.nextPath('/funscott')}>
-		            	Meet Fun Scott!
-		        	</button>
-		        </div>   
-		        <div className='workscott'>
-		        	<button className="wbttn" onClick={() => this.nextPath('/workscott')}>
-		           		Meet Work Scott!
-		        	</button> 
+		        <div className='center'>
+			    	<div className='hpic'>
+			      		<img src="https://s3.amazonaws.com/scott-personal-website/fun_and_work_scott.jpg"/>
+			        </div>
+			    	<br> 
+			   		</br>
+			        <div className='funscott'>
+			        	<button className="fbttn" onClick={() => this.nextPath('/funscott')}>
+			            	Meet Fun Scott!
+			        	</button>
+			        </div>   
+			        <div className='workscott'>
+			        	<button className="wbttn" onClick={() => this.nextPath('/workscott')}>
+			           		Meet Work Scott!
+			        	</button> 
+			        </div>
+			    	<br> 
+			   		</br>		        
+			        <div className='email'>
+			        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
+			        </div>
+			        <br> 
+			   		</br>
+				   	<div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
+				   	<div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
+				   	<div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
+			    </div>
+		    </MediaQuery>
+		    <MediaQuery maxDeviceWidth={899}>
+		        <div className='welcome-small'>
+		        	Hi, I'm Scott!
 		        </div>
-		    	<br> 
-		   		</br>		        
-		        <div className='email'>
-		        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
-		        </div>
-		        <br> 
-		   		</br>
-		   	<div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
-		   	<div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
-		   	<div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
-		    </div>
+		        <div className='center'>
+			    	<div className='pic-small'>
+			      		<img src="https://s3.amazonaws.com/scott-personal-website/fun_and_work_scott.jpg"/>
+			        </div>
+			    	<br> 
+			   		</br>
+			        <div className='funscott'>
+			        	<button className="fbttn" onClick={() => this.nextPath('/funscott')}>
+			            	Meet Fun Scott!
+			        	</button>
+			        </div>   
+			        <div className='workscott'>
+			        	<button className="wbttn" onClick={() => this.nextPath('/workscott')}>
+			           		Meet Work Scott!
+			        	</button> 
+			        </div>
+			    	<br> 
+			   		</br>		        
+			        <div className='email'>
+			        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
+			        </div>
+			        <br> 
+			   		</br>
+				   	<div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
+				   	<div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
+				   	<div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
+			    </div>
+		    </MediaQuery>
         </div>    
     )
   }
