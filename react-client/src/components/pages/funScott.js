95873b8e5f4f32342bbb2aa23b2efcb373fc4fa1
Scott Benson <scott.benson96@gmail.com>
2018-07-19 19:39:10 -0400

    make fun scott page responsive

--- react-client/src/components/pages/funScott.js
+++ react-client/src/components/pages/funScott.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import FaInstagram from 'react-icons/lib/fa/instagram';
 import FaTwitter from 'react-icons/lib/fa/twitter';
@@ -13,69 +14,136 @@ class FunScott extends Component {
   render() {
     return (
     	<div>
-    		<div className='center'>
-		    	<div className='welcome'>
-		        	Hi, I'm fun Scott!
-		        </div>
-		        <div className='pic'>
-					<img src="https://s3.amazonaws.com/scott-personal-website/fun_scott.jpg"/>
-				</div>
-				<br>
-				</br>
-				<div className='fun-intro'>
-					Nice to meet you! Want to get to know me better? Here's some fun facts about me! 
-				</div>
-			    <br>
-				</br>
-				<div className='fp-width'>
+    		<MediaQuery minDeviceWidth={900}>
+	    		<div className='center'>
+			    	<div className='welcome'>
+			        	Hi, I'm fun Scott!
+			        </div>
 			        <div className='pic'>
-				    	<img src="https://s3.amazonaws.com/scott-personal-website/spurs_scott.jpg"/>
-				 	</div>
-				</div>
-		        <div className='fun-description'>
-		      		I love playing and watching <b>soccer</b>. My favorite team is <b>Tottenham Hotspur</b>, a soccer team in the English Premier League.
-		        </div>
-		        <br> 
-		        </br>
-			    <div className='fp-width'>
-			    	<div className='pic'>
-				   		<img src="https://s3.amazonaws.com/scott-personal-website/britishscott.jpg"/>
-				    </div>
-				</div>
-		        <div className='fun-description'>
-		      		I’m half <b>British</b> and have <b>dual citizenship</b>! My dad’s from England, and is a huge reason for my love of soccer (football) and Tottenham Hotspur.
-		        </div>
-		        <br> 
-		        </br>
-		        <div className='fp-width'>
-			      	<div className='pic'>
-				    	<img src="https://s3.amazonaws.com/scott-personal-website/scottanimals.jpg"/>
-				    </div>
-				</div>
-		        <div className='fun-description'>
-		      		I love animals, especially <b>cats</b> and <b>dogs</b>. My family has four cats, even though I'm allergic to them and have to take a bunch of allergy medication. I also volunteer at a couple of animal shelters!
-		        </div>
-		        <br> 
-		        </br>
-		        <div className='fp-width'>
-		      		<div className='pic'>
-			   			 <img src="https://s3.amazonaws.com/scott-personal-website/scott76.jpg"/>
-			   		</div>
-			  	</div>
-		      <div className='fun-description'>
-		      	I have a love/hate relationship with the video game <b>Overwatch</b>. 
-		      </div>
-		        <br> 
-		   		</br>		        
-		        <div className='email'>
-		        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
-		        </div>
-		        <br> 
-		   		</br>
-		      <div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
-		   	  <div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
-		   	  <div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
-		    </div>
+						<img src="https://s3.amazonaws.com/scott-personal-website/fun_scott.jpg"/>
+					</div>
+					<br>
+					</br>
+					<div className='fun-intro'>
+						Nice to meet you! Want to get to know me better? Here's some fun facts about me! 
+					</div>
+				    <br>
+					</br>
+					<div className='fp-width'>
+				        <div className='pic'>
+					    	<img src="https://s3.amazonaws.com/scott-personal-website/spurs_scott.jpg"/>
+					 	</div>
+					</div>
+			        <div className='fun-description'>
+			      		I love playing and watching <b>soccer</b>. My favorite team is <b>Tottenham Hotspur</b>, a soccer team in the English Premier League.
+			        </div>
+			        <br> 
+			        </br>
+				    <div className='fp-width'>
+				    	<div className='pic'>
+					   		<img src="https://s3.amazonaws.com/scott-personal-website/britishscott.jpg"/>
+					    </div>
+					</div>
+			        <div className='fun-description'>
+			      		I’m half <b>British</b> and have <b>dual citizenship</b>! My dad’s from England, and is a huge reason for my love of soccer (football) and Tottenham Hotspur.
+			        </div>
+			        <br> 
+			        </br>
+			        <div className='fp-width'>
+				      	<div className='pic'>
+					    	<img src="https://s3.amazonaws.com/scott-personal-website/scottanimals.jpg"/>
+					    </div>
+					</div>
+			        <div className='fun-description'>
+			      		I love animals, especially <b>cats</b> and <b>dogs</b>. My family has four cats, even though I'm allergic to them and have to take a bunch of allergy medication. I also volunteer at a couple of animal shelters!
+			        </div>
+			        <br> 
+			        </br>
+			        <div className='fp-width'>
+			      		<div className='pic'>
+				   			 <img src="https://s3.amazonaws.com/scott-personal-website/scott76.jpg"/>
+				   		</div>
+				  	</div>
+			      <div className='fun-description'>
+			      	I have a love/hate relationship with the video game <b>Overwatch</b>. 
+			      </div>
+			        <br> 
+			   		</br>		        
+			        <div className='email'>
+			        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
+			        </div>
+			        <br> 
+			   		</br>
+			      <div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
+			   	  <div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
+			   	  <div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
+			    </div>
+			</MediaQuery>
+			<MediaQuery maxDeviceWidth={899}>
+	    		<div className='center'>
+			    	<div className='fun-welcome-small'>
+			        	Hi, I'm fun Scott!
+			        </div>
+			        <div className='pic'>
+						<img src="https://s3.amazonaws.com/scott-personal-website/fun_scott.jpg"/>
+					</div>
+					<br>
+					</br>
+					<div className='fun-intro-small'>
+						Nice to meet you! Want to get to know me better? Here's some fun facts about me! 
+					</div>
+				    <br>
+					</br>
+					<div className='fp-width'>
+				        <div className='pic'>
+					    	<img src="https://s3.amazonaws.com/scott-personal-website/spurs_scott.jpg"/>
+					 	</div>
+					</div>
+			        <div className='fun-description'>
+			      		I love playing and watching <b>soccer</b>. My favorite team is <b>Tottenham Hotspur</b>, a soccer team in the English Premier League.
+			        </div>
+			        <br> 
+			        </br>
+				    <div className='fp-width'>
+				    	<div className='pic'>
+					   		<img src="https://s3.amazonaws.com/scott-personal-website/britishscott.jpg"/>
+					    </div>
+					</div>
+			        <div className='fun-description'>
+			      		I’m half <b>British</b> and have <b>dual citizenship</b>! My dad’s from England, and is a huge reason for my love of soccer (football) and Tottenham Hotspur.
+			        </div>
+			        <br> 
+			        </br>
+			        <div className='fp-width'>
+				      	<div className='pic'>
+					    	<img src="https://s3.amazonaws.com/scott-personal-website/scottanimals.jpg"/>
+					    </div>
+					</div>
+			        <div className='fun-description'>
+			      		I love animals, especially <b>cats</b> and <b>dogs</b>. My family has four cats, even though I'm allergic to them and have to take a bunch of allergy medication. I also volunteer at a couple of animal shelters!
+			        </div>
+			        <br> 
+			        </br>
+			        <div className='fp-width'>
+			      		<div className='pic'>
+				   			 <img src="https://s3.amazonaws.com/scott-personal-website/scott76.jpg"/>
+				   		</div>
+				  	</div>
+			      <div className='fun-description'>
+			      	I have a love/hate relationship with the video game <b>Overwatch</b>. 
+			      </div>
+			        <br> 
+			   		</br>		        
+			        <div className='email'>
+			        	Wanna talk? You can find me at <b>scottbenson96@gmail.com</b> OR
+			        </div>
+			        <br> 
+			   		</br>
+			      <div className="socButton"><a href="https://www.instagram.com/scott_benson_/" target="_blank"><FaInstagram/></a></div>
+			   	  <div className="socButton"><a href="https://twitter.com/scott_benson_" target="_blank"><FaTwitter/></a></div>
+			   	  <div className="socButton"><a href="https://www.linkedin.com/in/scott-benson1/" target="_blank"><FaLinkedIn/></a></div>
+			    </div>
+			</MediaQuery>
         </div>    
     )
   }
