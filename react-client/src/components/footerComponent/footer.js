c1f3bcc490afce483ba86e84fa49848f34d81554
Scott Benson <scott.benson96@gmail.com>
2018-07-19 18:28:46 -0400

    more responsive hompage, navbar, and footer

--- react-client/src/components/footerComponent/footer.js
+++ react-client/src/components/footerComponent/footer.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <div>
-          Copyright © 2018 Scott Benson. All Rights Reserved. 
-        </div>
-      </footer>
+    	<div>
+	        <MediaQuery minDeviceWidth={900}>
+		       	<footer>
+		        	<div>
+		           		Copyright © 2018 Scott Benson. All Rights Reserved. 
+		        	</div>
+	        	</footer>
+	        </MediaQuery>
+	        <MediaQuery maxDeviceWidth={899}>
+		       	<div className='footer-small'>
+		        	<div>
+		           		Copyright © 2018 Scott Benson. All Rights Reserved. 
+		        	</div>
+	        	</div>
+	        </MediaQuery>
+	    </div>
     )
   }
 }
--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 import FaHome from 'react-icons/lib/fa/home';
 
@@ -6,9 +7,16 @@ class NavBar extends Component {
   render() {
     return (
       <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to=""><FaHome/></Link></li>
-        </ul>
+      	<MediaQuery minDeviceWidth={900}>
+	        <ul id="headerButtons">
+	          <li className="navButton"><Link to=""><FaHome/></Link></li>
+	        </ul>
+	    </MediaQuery>
+	    <MediaQuery maxDeviceWidth={899}>
+	        <ul id="headerButtons">
+	          <li className="navButton-small"><Link to=""><FaHome/></Link></li>
+	        </ul>
+	    </MediaQuery>
       </header>
     )
   }
